b92c5bb00bc2f4ab3e7a5320a571a4ba70671617
Ivan Oreshnikov <me@tma.localdomain>
2018-03-30 10:52:30 +0300

    Added support for VPN networks.

--- network.py
+++ network.py
@@ -7,6 +7,9 @@ from subprocess import check_output
 ICON_WIFI = "<fn=1>\uf1eb</fn>"
 TYPE_WIFI = "802-11-wireless"
 
+ICON_VPN = "<fn=1>\uf023</fn>"
+TYPE_VPN = "vpn"
+
 
 command = "nmcli -t connection show --active"
 status = check_output(command.split())
@@ -15,7 +18,22 @@ status = status.decode("utf-8").strip()
 if not status:
     exit()
 
-name, uuid, type_, device = status.split(":")
+connections = status.splitlines()
+indicators = []
 
-if type_ == TYPE_WIFI:
-    print("%s  %s" % (ICON_WIFI, name))
+for connection in connections:
+    name, uuid, type_, device = connection.split(":")
+
+    if type_ not in [TYPE_WIFI, TYPE_VPN]:
+        continue
+
+    if type_ == TYPE_WIFI:
+        icon = ICON_WIFI
+    if type_ == TYPE_VPN:
+        icon = ICON_VPN
+
+    indicator = "%s  %s" % (icon, name)
+    indicators.append(indicator)
+
+
+print("    ".join(indicators))
